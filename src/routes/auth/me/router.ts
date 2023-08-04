e626f315831a2992e7940ee811ac9df17871684c
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-04 23:18:05 +0200

    created a new api endpoint to change user data

--- src/routes/auth/me/router.ts
+++ src/routes/auth/me/router.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import getDataRouter from './getData';
 import deleteRouter from './delete';
+import patchDataRouter from './changeData';
 
 const router = express.Router();
 
 router.use('/', getDataRouter);
 router.use('/', deleteRouter);
+router.use('/', patchDataRouter);
 
 export default router;
