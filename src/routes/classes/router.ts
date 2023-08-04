435f48585aebb62243d9f9546d045045d6a2708d
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-04 16:36:55 +0200

    added a route to get all classes from a school

--- src/routes/classes/router.ts
+++ src/routes/classes/router.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+
 import createClassRouter from './createClass';
+import getClassRouter from './getClass';
 
 const router = express.Router();
 
 router.use('/', createClassRouter);
+router.use('/', getClassRouter);
 router.all('/', (req, res) => {
     res.status(405).json({
         status: 'error',
