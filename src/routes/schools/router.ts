1c55ff7c45c5a2e3ad610c714b5140963c3db4d1
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-05 13:00:37 +0200

    added a route to get details about a specific school

--- src/routes/schools/router.ts
+++ src/routes/schools/router.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 import getSchoolRouter from './getSchools';
 import createSchoolRouter from './createSchool';
+import getSpecificSchoolRouter from './getSpecificSchool';
 
 const router = express.Router();
 
 router.use('/', createSchoolRouter);
 router.use('/', getSchoolRouter);
+router.use('/', getSpecificSchoolRouter);
 router.all('/', (req, res) => {
     res.status(405).json({
         status: 'error',
