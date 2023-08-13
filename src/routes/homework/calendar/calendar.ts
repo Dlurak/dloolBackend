d81c2225c5e9447997a6348ab628a507fc226366
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-13 17:06:14 +0200

    documented the new route

--- src/routes/homework/calendar/calendar.ts
+++ src/routes/homework/calendar/calendar.ts
@@ -1,5 +1,8 @@
 import express from 'express';
-import { getHomeworkForMultipleClasses, getHomeworkForUser } from '../../../database/homework/findHomework';
+import {
+    getHomeworkForMultipleClasses,
+    getHomeworkForUser,
+} from '../../../database/homework/findHomework';
 
 import ical, { ICalEventTransparency } from 'ical-generator';
 import { findUniqueSchool } from '../../../database/school/findSchool';
@@ -8,6 +11,34 @@ import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 
+/**
+ * @api {get} /homework/calendar/:school Get homework calendar for a school
+ * @apiName GetHomeworkCalendar
+ * @apiGroup Homework
+ * @apiDescription Get a calendar with all homework for a school in iCal format. IT IS NOT JSON!!!
+ * @apiVersion 1.1.0
+ *
+ * @apiParam {String} school The unique name of the school to get the calendar for
+ * @apiQuery {String[]} classes A comma separated list of classes to get the calendar for
+ *
+ * @apiSuccess (200) {String} The iCal calendar, in text/calendar format <b>NOTE THAT THIS IS NOT JSON!!!</b>
+ *
+ * @apiError (404) {String} status The status of the response
+ * @apiError (404) {String} message The error message
+ *
+ * @apiErrorExample {json} 404 - School not found:
+ *   HTTP/1.1 404 Not Found
+ *   {
+ *     "status": "error",
+ *     "message": "School not found"
+ *  }
+ * @apiErrorExample {json} 404 - No classes found:
+ *   HTTP/1.1 404 Not Found
+ *   {
+ *     "status": "error",
+ *     "message": "No classes found"
+ *   }
+ */
 router.get('/calendar/:school', async (req, res) => {
     const school = req.params.school;
     const rawClasses = req.query.classes;
@@ -23,11 +54,15 @@ router.get('/calendar/:school', async (req, res) => {
     }
 
     const classIdsPromises = classes.map((className: string) => {
-        const class_ = classesCollection.findOne({ name: className, school: schoolObj._id }).then((class_) => class_?._id ?? null);
-        return class_
+        const class_ = classesCollection
+            .findOne({ name: className, school: schoolObj._id })
+            .then((class_) => class_?._id ?? null);
+        return class_;
     });
 
-    const classIds = (await Promise.all(classIdsPromises)).filter((id) => id !== null);
+    const classIds = (await Promise.all(classIdsPromises)).filter(
+        (id) => id !== null,
+    );
 
     if (classIds.length === 0) {
         return res.status(404).json({
@@ -36,7 +71,9 @@ router.get('/calendar/:school', async (req, res) => {
         });
     }
 
-    const homework = await getHomeworkForMultipleClasses(classIds as ObjectId[]);
+    const homework = await getHomeworkForMultipleClasses(
+        classIds as ObjectId[],
+    );
 
     const cal = ical({
         name: 'Homework',
@@ -44,7 +81,11 @@ router.get('/calendar/:school', async (req, res) => {
 
     homework.forEach((hw) => {
         hw.assignments.forEach((assignment) => {
-            const dueDate = new Date(assignment.due.year, assignment.due.month - 1, assignment.due.day);
+            const dueDate = new Date(
+                assignment.due.year,
+                assignment.due.month - 1,
+                assignment.due.day,
+            );
             cal.createEvent({
                 start: dueDate,
                 end: dueDate,
@@ -53,13 +94,13 @@ router.get('/calendar/:school', async (req, res) => {
                 summary: assignment.subject,
                 description: assignment.description,
 
-                transparency: ICalEventTransparency.TRANSPARENT
-            })
+                transparency: ICalEventTransparency.TRANSPARENT,
+            });
         });
-    })
-    
+    });
+
     res.set('Content-Type', 'text/calendar');
-    res.status(200).send(cal.toString())
+    res.status(200).send(cal.toString());
 });
 
 export default router;
