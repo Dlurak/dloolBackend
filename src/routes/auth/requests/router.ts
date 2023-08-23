d8562ad535843c2ea2d4ba6ea140b079f5a87b4a
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-23 18:43:42 +0200

    added a route to get all requests relevant for a given a user

--- src/routes/auth/requests/router.ts
+++ src/routes/auth/requests/router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import getSpecificRequestRouter from './getSpecificRequest';
+import getListOfRequestsRouter from './listRequest';
 
 const router = express.Router();
 
 router.use('/', getSpecificRequestRouter);
+router.use('/', getListOfRequestsRouter);
 
 export default router;
